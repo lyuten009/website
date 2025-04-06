4c05cf055ba7ab3f1b7b32fe9b8b58098144a698
lyuten009 <lowqualityjpgofalobster@gmail.com>
2025-04-06 13:30:45 -0400

    Test

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    let { children } = $props();
+    import "./style.css";
+</script>
+<center>
+    <div class = "container">
+        <aside>
+            <!-- FC2 Counter  Starts here -->
+                <!-- svelte-ignore a11y_distracting_elements -->
+                <marquee direction = "right">
+                    <!-- svelte-ignore a11y_missing_attribute -->
+                    <!-- svelte-ignore a11y_missing_attribute -->
+                    <script language="javascript" type="text/javascript" src="//counter1.fc2.com/counter.php?id=89510396"></script><noscript><img src="//counter1.fc2.com/counter_img.php?id=89510396" /></noscript>
+                </marquee>
+            <!-- FC2 Counter  Ends here -->
+            <h3>vote my children</h3>
+            <form method="post" action="https://poll.pollcode.com/88266659">
+                <div style="background-color:#EEEEEE;padding:2px;width:175px;font-family:Palatino Linotype;font-size:small;color:#B8BA7E;background-image: url('https://images.boardhost.com/bg/leather.gif');">
+                    <div style="padding:2px 0px 4px 2px;">
+                        <strong>opinions on the new look</strong>
+                    </div>
+                    <input type="radio" name="answer" value="1" id="answer882666591" style="float:left;" />
+                    <label for="answer882666591" style="float:left;width:150px;">ew ew ew ugly</label>
+                    <div style="clear:both;height:2px;"></div>
+                    <input type="radio" name="answer" value="2" id="answer882666592" style="float:left;" />
+                    <label for="answer882666592" style="float:left;width:150px;">ok</label>
+                    <div style="clear:both;height:2px;"></div>
+                    <div align="center" style="padding:3px;">
+                        <input type="submit" value=" Vote ">&nbsp;
+                        <input type="submit" name="view" value=" View ">
+                    </div>
+                    <div align="right" style="font-size:10px">pollcode.com <a href="https://pollcode.com/">free polls</a>
+                    </div>
+                </div>
+            </form>
+            <!-- FC2 Counter  Starts here -->
+                <!-- svelte-ignore a11y_distracting_elements -->
+                <marquee>
+                    <!-- svelte-ignore a11y_missing_attribute -->
+                    <script language="javascript" type="text/javascript" src="//counter1.fc2.com/views.php?id=89510396"></script><noscript><img src="//counter1.fc2.com/counter_now.php?id=89510396" /></noscript>
+                </marquee>
+            <!-- FC2 Counter  Ends here -->
+        </aside>
+        <main>
+            <header>
+                <!--<span>
+                    <div id="statuscafe">
+                        <div id="statuscafe-username">
+                        </div>
+                        <div id="statuscafe-content">
+                        </div>
+                    </div>
+                    <script src="https://status.cafe/current-status.js?name=2254" defer></script>
+                </span>-->
+                <nav>
+                    <span>
+                        <a href = "/">home</a>
+                    </span>
+                    <span>
+                        <a href = "/">about</a>
+                        <a href = "/">diary</a>
+                        <a href = "/">gallery</a>
+                        <a href = "/">wrld</a>
+                        <a href = "/">offsite</a>
+                        <s><a href = "/">guestbook</a></s>
+                    </span>
+                </nav>
+            </header>
+            <article>
+                {@render children()}
+            </article>
+            <footer>
+                <span>This site is not copyrighted.</span>
+                <span>last active : 06-04-25</span>
+            </footer>
+        </main>
+    </div>
+</center>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<section>
+    <h2>welcome</h2>
+    <p>
+        Hello and welcome to my website. It's perpetually under construction. It should
+        work, display, and function properly on phones but I can't promise
+        anything...
+        I'd say it's best viewed in a Chromium browser but Firefox should be ok too.
+        <br>
+        Also fuck tablet users you don't get anything special. (Use landscape mode)
+        <br>
+        Thank you for visiting and enjoy your stay! (or don't)
+    </p>
+</section>
+<section>
+    <h2>featured</h2>
+    <div>
+        <figure class = "float-right">
+            <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Heterohyus_4939.jpg/800px-Heterohyus_4939.jpg" alt = "placeholder">
+            <figcaption>placeholder...</figcaption>
+        </figure>
+        <p>
+            I don't have anything to put here really BUT this skeleton looks pretty cool.
+            It is a... uh... "Euarchontoglire", which is the group where Treeshrews, Colugos,
+            Primates, and Glires come together. I've been looking at the recently for
+            worldbuilding purposes because I'm actually awesome and super cool.
+            Originally I had one of the sophonts descend from rabbits but then I remembered
+            that I don't want people to think I'm a furry so I went with making them weird
+            Euarchontoglire descendants instead. Because then I can say they're close enough
+            to monkies or something. I also wanted them to have canines. As in canine teeth.
+            Because canine teeth are pretty awesome yk. Canine teeth as in the canines not as
+            in dogs. I also can't really get this to float how I want it to float so that's also
+            why it's here. For testing purposes. Yay. They're euthanising me on Wednesday.
+        </p>
+    </div>
+</section>
